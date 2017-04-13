a5d36503557b9e57ab1f97cb9750f5eb02c5d5fb
Srinidhi G S <srini.satti+lwt@gmail.com>
2017-04-14 00:46:06 +0530

    get company revenue

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,23 @@
+require 'rubygems'
+require 'mechanize'
+
 class WelcomeController < ApplicationController
 
 	def index
+		@revenue = params[:revenue] if params[:revenue].present?
+		@company_name = params[:company] if params[:company].present?
+	end
+
+	def search_results
+		byebug
+		agent = Mechanize.new
+		company_name = params[:search_string]
+		page = agent.get("http://www.google.com?q=#{company_name}+revenue")
+		form = page.form_with(:action => '/search')
+		search_results = form.submit
+		revenue = search_results.at('.g').children.text
+		redirect_to root_path(company: company_name,revenue: revenue)
 	end
 end
+
+
